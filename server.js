2e8c026e49a364eec560a9d7ab5c4c79d9803eb3
Gene Kobilansky <genek149@gmail.com>
2016-08-03 17:16:53 +0000

    added giphy search

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
+//var Promise = require('promise');
+var giphy = require('giphy-api')();
+
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
+});
+
+app.get('/api/gifsearch/*', function(req, res) {
+    
+    var searchResults;
+    
+    giphy.search('pokemon', function(err, res) {
+    // Res contains gif data!
+    if (err) return res(err);
+    
+    searchResults = res;
+    
+    console.log(res);
+    });
+    
+    res.send(searchResults);
 });
 
 app.listen(8080, function() {
