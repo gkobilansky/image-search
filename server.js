526f5a06dc1b75f26f26d28715abe79312ddca3b
Gene Kobilansky <genek149@gmail.com>
2016-08-04 19:23:18 +0000

    added search functionality and latest route

--- server.js
+++ server.js
@@ -8,20 +8,28 @@ app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-app.get('/api/gifsearch/*', function(req, res) {
+app.get('/api/latest/gifsearch', function(req, res) {
+    res.send('Latest searches');
+});
+
+app.get('/api/gifsearch/:search', function(req, res) {
     
-    var searchResults;
+    var search = req.params.search,
+        offset = req.query.offset,
+        searchResults;
+        
     
-    giphy.search('pokemon', function(err, res) {
+    giphy.search({
+        q: search,
+        offset: offset
+    }, function(err, res) {
     // Res contains gif data!
     if (err) return res(err);
-    
     searchResults = res;
-    
-    console.log(res);
+    console.log(offset, searchResults);
     });
     
-    res.send(searchResults);
+    res.send('search complete');
 });
 
 app.listen(8080, function() {
