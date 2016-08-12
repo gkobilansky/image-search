1773fcc381d2d7a17bf6d41171f4354df7d30e74
Gene Kobilansky <genek149@gmail.com>
2016-08-12 16:51:06 +0000

    sending results to browser!

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
-//var Promise = require('promise');
+var MongoClient = require('mongodb').MongoClient;
+var db;
+var url = 'mongodb://localhost:27017/mydatabase';
+var searchResults;
 var giphy = require('giphy-api')();
+
 
 
 app.get('/', function(req, res) {
@@ -9,29 +13,42 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api/latest/gifsearch', function(req, res) {
-    res.send('Latest searches');
+        db.collection('searches').find().toArray(function(err, results) {
+            if (err) throw err
+             console.log(results)
+        });
+        
+        res.redirect('/');
 });
 
-app.get('/api/gifsearch/:search', function(req, res) {
+app.get('/api/gifsearch/:search', function(req, res, next) {
     
     var search = req.params.search,
-        offset = req.query.offset,
-        searchResults;
+        offset = req.query.offset;
         
-    
+    db.collection('searches').insert({"term":search,"when": new Date()});    
+        
     giphy.search({
         q: search,
-        offset: offset
+        offset: offset,
+        limit: 10
     }, function(err, res) {
-    // Res contains gif data!
-    if (err) return res(err);
-    searchResults = res;
-    console.log(offset, searchResults);
+      // Res contains gif data!
+        if (err) return res(err);
+        searchResults = res;
+        console.log(offset, typeof searchResults);
+        next(); 
     });
-    
-    res.send('search complete');
+    }, function (req, res) {    
+         console.log(typeof searchResults);
+         res.send(searchResults);
+    });
+
+MongoClient.connect(url, function(err, database) {
+      if (err) throw err;
+      db = database;    
+      app.listen(8080, function () {
+        console.log('Gif Search API listening on port 8080!');
+      });
 });
 
-app.listen(8080, function() {
-    console.log('Hello world listening on port 8080');
-});
